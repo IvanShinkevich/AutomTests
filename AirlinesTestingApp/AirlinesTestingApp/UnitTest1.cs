7608aec0a3824d714d4b4425ae53d48313bfc928
IvanShinkevich <2014ivanee@gmail.com>
2018-10-28 16:38:37 +0300

    Added base selenium test

---
+++ AirlinesTestingApp/AirlinesTestingApp/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AirlinesTestingApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Init driver
+            var driver = new ChromeDriver();
+            //Navigate to tab
+            driver.Navigate().GoToUrl("https://www.aircaraibes.com/");
+            //Close ads
+            driver.FindElementByClassName("optanon-alert-box-close").Click();
+            //Click checkbox for one-way ticket
+            Thread.Sleep(1000);
+            var chkBox = driver.FindElement(By.Id("departure-only"));
+            chkBox.Click();
+            //Assert that "date from" is disabled
+            var el = driver.FindElementById("edit-b-date-2-booking-0");
+            if (el.Enabled != false)
+            {
+                throw new AssertFailedException();
+            }
+            var el1 = driver.FindElementById("uniform-edit-date-range-value-2");
+            var classNames = el1.GetAttribute("className");
+            if (!classNames.Contains("disabled"))
+            {
+                throw new AssertFailedException();
+            }
+        }
+    }
+}
