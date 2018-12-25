2390e8ccc28579ad78cfc3b621303a240620e39f
IvanShinkevich <2014ivanee@gmail.com>
2018-12-25 21:07:18 +0300

    updated driver class a bit

--- AirlinesTestingApp/AirlinesTestingApp/BaseEntities/Driver.cs
+++ AirlinesTestingApp/AirlinesTestingApp/BaseEntities/Driver.cs
@@ -3,23 +3,23 @@ using OpenQA.Selenium.Chrome;
 
 namespace AirlinesTestingApp.BaseEntities
 {
-    public static class Driver
+    public class Driver
     {
         private static IWebDriver driver;
+
+        private Driver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
 
         public static IWebDriver GetDriverInstance()
         {
             if(driver == null)
             {
-                SetupChromeDriver();
+                new Driver();
             }
             return driver;
-        }
-
-        public static void SetupChromeDriver()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
         }
 
         public static void QuitDriver()
